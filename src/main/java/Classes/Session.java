15b5f5a3b51008787abd60788fcf4c7558f41830
Aditya V Braganza <aditya.braganza@student.manchester.ac.uk>
2026-03-12 17:01:18 +0000

    Finished Session V1

--- src/main/java/Classes/Session.java
+++ src/main/java/Classes/Session.java
@@ -3,76 +3,95 @@ package Classes;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
-import java.util.stream.Collectors;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import javax.net.ssl.*;
+
 public class Session {
     private final String root_url;
-    private final String session_id;
+    private String session_id;
     private final String base64Auth;
-    private HttpURLConnection subscribed_connection;
+    private HttpsURLConnection subscribed_connection;
+    private BufferedReader main_br;
 
     public Session(String ip, String username, String password) throws Exception {
+        disableSslVerification();
         this.root_url = "https://" + ip + ":443/api";
         this.base64Auth = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
 
-        HttpURLConnection start_session_connection = create_http_url_connection(this.root_url + "/ssc/state/subscriptions", "GET");
-        int start_session_status = start_session_connection.getResponseCode();
+        this.subscribed_connection = create_https_url_connection(this.root_url + "/ssc/state/subscriptions", "GET", 2000, 0);
+        int start_session_status = this.subscribed_connection.getResponseCode();
         if (start_session_status != 200){
-            BufferedReader br = new BufferedReader((new InputStreamReader(start_session_connection.getErrorStream())));
+            this.main_br = new BufferedReader((new InputStreamReader(this.subscribed_connection.getErrorStream())));
             String inputLine;
             StringBuilder content = new StringBuilder();
-            while ((inputLine = br.readLine()) != null){
+            while ((inputLine = this.main_br.readLine()) != null){
                 content.append(inputLine);
                 // Write code to raise an exception with the text of error message defining the return text
             }
-            br.close();
+            this.main_br.close();
         }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(start_session_connection.getInputStream()));
-        JsonObject start_session_json = JsonParser.parseString(br.lines().collect(Collectors.joining("\n"))).getAsJsonObject();
-        this.session_id = start_session_json.get("sessionUUID").getAsString();
-        // Write exception code
+        this.main_br = new BufferedReader(new InputStreamReader(this.subscribed_connection.getInputStream()));
+        String line;
+        boolean continue_start_session_read = true;
+        while ((line = this.main_br.readLine()) != null && continue_start_session_read){
+            if (!line.trim().isEmpty() && line.startsWith("data:")) {
+                JsonObject start_session_json = JsonParser.parseString(line.substring(5).trim()).getAsJsonObject();
+                this.session_id = start_session_json.get("sessionUUID").getAsString();
+                continue_start_session_read = false;
+            }
+        }
     }
 
-    public void subscribe() throws Exception {
-        subscribe(new String[]{"/"});
-        // Continue exception code
+    public void subscribe(String payload) throws Exception {
+        subscribe(new String[]{payload});
     }
 
     public void subscribe(String[] payloads) throws Exception {
-        StringBuilder payload_string = new StringBuilder("{\"paths\":[\"");
+        StringBuilder payload_string = new StringBuilder("[");
         for (int i = 0; i < payloads.length; i++){
             payload_string.append("\"").append(payloads[i]).append("\"");
             if (i < payloads.length - 1){
                 payload_string.append(",");
             }
         }
-        payload_string.append("\"]}");
-        this.subscribed_connection = create_http_url_connection(this.root_url + "/ssc/state/subscriptions/" + this.session_id, "PUT");
-        this.subscribed_connection.setRequestProperty("Content-Type", "application/json");
-        this.subscribed_connection.setDoOutput(true);
-        try (OutputStream os = this.subscribed_connection.getOutputStream()) {
+        payload_string.append("]");
+        HttpsURLConnection subscribing_https_url_connection = create_https_url_connection(this.root_url + "/ssc/state/subscriptions/" + this.session_id, "PUT");
+        subscribing_https_url_connection.setRequestProperty("Content-Type", "application/json");
+        subscribing_https_url_connection.setDoOutput(true);
+        try (OutputStream os = subscribing_https_url_connection.getOutputStream()) {
             byte[] input = payload_string.toString().getBytes(StandardCharsets.UTF_8);
             os.write(input, 0, input.length);
         }
-        int responseCode = this.subscribed_connection.getResponseCode();
-        System.out.println("Response Code: " + responseCode);
+        int responseCode = subscribing_https_url_connection.getResponseCode();
         if (responseCode != 200){
+            System.out.println("1: " + responseCode);
             //Write code to throw an exception
         }
         // Write exception code
     }
 
+    public JsonObject get_subscribed_content() throws Exception{
+        String line;
+        while ((line = this.main_br.readLine()) != null){
+            if (!line.trim().isEmpty() && line.startsWith("data:")) {
+                return JsonParser.parseString(line.substring(5).trim()).getAsJsonObject();
+            }
+        }
+        return null;
+        // Write exception code
+    }
+
     public void end() throws Exception{
-        HttpURLConnection end_connection = create_http_url_connection(this.root_url + "/ssc/state/subscriptions", "DELETE", 5000, 2000);
+        HttpsURLConnection end_connection = create_https_url_connection(this.root_url + "/ssc/state/subscriptions/" + this.session_id, "DELETE", 5000, 5000);
         int end_status = end_connection.getResponseCode();
         if (end_status != 200){
             // Write code to raise an exception with status code and respective error
@@ -81,9 +100,9 @@ public class Session {
         // Write exception management for connection
     }
 
-    private HttpURLConnection create_http_url_connection(String url, String request_method, int connect_timeout, int read_timeout) throws Exception{
+    private HttpsURLConnection create_https_url_connection(String url, String request_method, int connect_timeout, int read_timeout) throws Exception{
         URL connection_url = new URI(url).toURL();
-        HttpURLConnection connection = (HttpURLConnection) connection_url.openConnection();
+        HttpsURLConnection connection = (HttpsURLConnection) connection_url.openConnection();
         connection.setRequestMethod(request_method);
         connection.setConnectTimeout(connect_timeout);
         connection.setReadTimeout(read_timeout);
@@ -92,8 +111,39 @@ public class Session {
         // Write exception code
     }
 
-    private HttpURLConnection create_http_url_connection(String url, String request_method) throws Exception{
-        return create_http_url_connection(url, request_method, 2000, 5000);
+    private HttpsURLConnection create_https_url_connection(String url, String request_method) throws Exception{
+        return create_https_url_connection(url, request_method, 2000, 5000);
         // Continue exception code
+    }
+
+    public static void disableSslVerification() {
+        try {
+            // Create a trust manager that blindly accepts all certificates
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() { return null; }
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            // Create an all-trusting host name verifier
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+
+            // Install the all-trusting host verifier
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        } catch (Exception e) {
+            System.out.println("Failed to bypass SSL: " + e.getMessage());
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,22 @@
+import Classes.Session;
+import com.google.gson.JsonObject;
+
 void main() {
-    System.out.println("Test");
+    try{
+        Session session = new Session("10.10.20.251", "api", "ParadiseGreen1!");
+        session.subscribe("/api/channel/0/level");
+        while (true){
+            JsonObject obj = session.get_subscribed_content();
+            if (obj != null){
+                System.out.println(obj);
+            } else{
+                System.out.println("uh oh");
+            }
+        }
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+        for (StackTraceElement i: e.getStackTrace()){
+            System.out.println(i);
+        }
+    }
 }
